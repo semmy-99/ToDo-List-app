0a55e07f715f057877b64ea2a5d72aa9f0086abb
semmy-99 <oluwasemiloretowoba@gmail.com>
2022-05-09 11:19:58 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Oluwasemilore on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
